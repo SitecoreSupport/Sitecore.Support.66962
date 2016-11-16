5d5ff97bbaa9016d88cf811321fed6cbdd42b8f0
Ivan Sheyenko <ivs@sitecore.net>
2016-11-16 16:22:34 +0200

    Initial patch commit

---
+++ src/Sitecore.Support.66962/CheckBoxListField.cs
@@ -0,0 +1,27 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.Forms.Mvc.Models.Fields;
+using Sitecore.Support.Forms.Mvc.Validators;
+using System;
+
+namespace Sitecore.Support.Forms.Mvc.Models.Fields
+{
+    public class CheckBoxListField : Sitecore.Forms.Mvc.Models.Fields.CheckBoxListField
+    {
+        [DynamicRequiredCheckBoxList("IsRequired", ErrorMessage = "The {0} field is required.")]
+        public override object Value
+        {
+            get
+            {
+                return base.Value;
+            }
+            set
+            {
+                base.Value = value;
+            }
+        }
+
+        public CheckBoxListField(Item item) : base(item)
+        {
+        }
+    }
+}
---
+++ src/Sitecore.Support.66962/RadioListField.cs
@@ -0,0 +1,29 @@
+﻿using Sitecore.Support.Forms.Mvc.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.Forms.Mvc.Models.Fields
+{
+    public class RadioListField : Sitecore.Forms.Mvc.Models.Fields.RadioListField
+    {
+        public RadioListField(Sitecore.Data.Items.Item item) : base(item)
+        {
+        }
+
+        [DynamicRequiredCheckBoxList("IsRequired", ErrorMessage = "The {0} field is required.")]
+        public override object Value
+        {
+            get
+            {
+                return base.Value;
+            }
+
+            set
+            {
+                base.Value = value;
+            }
+        }
+    }
+}
